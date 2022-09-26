af683dce3a0ffe28f4508d58a3a1bc73f8b761b0
‘whl’ <18231213@buaa.edu.cn>
2022-09-26 20:11:04 +0800

    feature(whl): script for batch eval

--- batch_eval.sh
+++ batch_eval.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#run experiements with different level of non-iid
 path_prefix=./logging/cifar10_resnet18_dirichlet_fedavg_alpha_
 
 for ((i=2;i<=8;i+=2))
 do
-        echo "python run.py --log_dir"$path_prefix"0.$i --alpha 0.$i"
+        python run.py --logging_path $path_prefix0.$i --alpha 0.$i
 done
