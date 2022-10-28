5ea5e6f79b03edb83cc796e6df322e45a1f0df69
‘whl’ <18231213@buaa.edu.cn>
2022-10-28 14:23:08 +0800

    polish config

--- args/test_arg.py
+++ args/test_arg.py
@@ -5,14 +5,14 @@ def args_parser():
     parser = argparse.ArgumentParser()
     # federated arguments
     parser.add_argument('--loc_eps', type=int, default=10, help="rounds of training")
-    parser.add_argument('--glob_eps', type=int, default=120, help="global training round")
+    parser.add_argument('--glob_eps', type=int, default=200, help="global training round")
     parser.add_argument('--client_num', type=int, default=30, help="number of client")
     parser.add_argument('--client_sample_rate', type=float, default=1, help="client_sample_rate")
     parser.add_argument('--decay_factor', type=float, default=0.97, help="decay factor of learning rate")
     parser.add_argument('--aggr_method', type=str, default='avg', help='aggregation method')
     parser.add_argument('--fed_dict', type=str, default='all', help='only keys in this will use fed-learning')
     parser.add_argument('--sample_method', type=str, default='dirichlet', help="method for sampling")
-    parser.add_argument('--alpha', type=float, default=0.8, help="alpha for dirichlet distribution")
+    parser.add_argument('--alpha', type=float, default=0.2, help="alpha for dirichlet distribution")
 
     # training arguments
     parser.add_argument('--batch_size', type=int, default=32, help="batch_size")
@@ -35,7 +35,7 @@ def args_parser():
     parser.add_argument('--resize', type=int, default=-1, help='resize the input image, -1 means no resizing')
 
     # logging and evaluation
-    parser.add_argument('--test_freq', type=int, default=1, help="rounds of testing")
+    parser.add_argument('--test_freq', type=int, default=5, help="rounds of testing")
     parser.add_argument('--logging_path', type=str, default='./logging/avg_round10', help='logging path')
 
     args = parser.parse_args()
