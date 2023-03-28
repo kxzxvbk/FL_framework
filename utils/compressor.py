10a58e358dd4c2ae0ba5bea4fd3f8d649889bb2c
‘whl’ <18231213@buaa.edu.cn>
2023-03-28 22:40:41 +0800

    update resnet

--- utils/compressor.py
+++ utils/compressor.py
@@ -22,7 +22,7 @@ def check_avg(clients, server, tb_logger):
     cnt += 1
     deal_keys = []
     for k in server['glob_dict'].keys():
-        if len(server['glob_dict'][k].shape) == 4:
+        if len(server['glob_dict'][k].shape) in [2, 4]:
             deal_keys.append(k)
 
     state_dicts = [client.model.state_dict() for client in clients]
